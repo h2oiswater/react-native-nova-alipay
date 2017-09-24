b8ee69a8115b16f59dd3d59f97925c4ad3d4f937
micjoyce <michael_xu@caifujilu.com>
2017-09-24 20:32:16 +0800

    fix deprecated RN 0.47

--- android/src/main/java/cn/idealsense/reactnative/alipay/AlipayPackage.java
+++ android/src/main/java/cn/idealsense/reactnative/alipay/AlipayPackage.java
@@ -23,7 +23,7 @@ public final class AlipayPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // Deprecated RN 0.47
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
